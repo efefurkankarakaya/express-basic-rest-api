e591095dbbb463a0fc79563e9c29346a3c9307a1
efefurkankarakaya <efkdev@outlook.com>
2020-08-13 16:57:11 +0300

    random user generating added

--- express.js
+++ express.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const tools = require("./tools");
 
 app.get("/users/:id", (req, res) => { // localhost:3000/users/4
     fs.readFile("users.json", "utf8", (err, data) => {
         res.set({
             "content-type" : "application/json"
-        })
+        });
         JSON.parse(data)[req.params.id] ? 
         res.end(JSON.stringify(JSON.parse(data)[req.params.id])) :
         res.end("{}");
@@ -17,7 +18,7 @@ app.get(`/users`, (req, res) => {
     fs.readFile("users.json", "utf8", (err, data) => {
         res.set({
             "content-type" : "application/json"
-        })
+        });
         res.end(data);
     });
 });
@@ -30,12 +31,12 @@ app.get("/add", (req, res) => {
             "name" : req.query.name,
             "password" : req.query.password,
             "email" : req.query.email
-        } 
+        };
         const length = data ? Object.keys(data).length : 0;
         data[length] = json_struct;
         res.set({
             "content-type" : "application/json"
-        })
+        });
         res.end(JSON.stringify(data[length])); // end needs buffer or string as param
         fs.writeFile("users.json", JSON.stringify(data), "utf8", (err) => console.log(err));
     });
@@ -60,23 +61,42 @@ app.get("/edit", (req, res) => {
             "name" : req.query.name ? req.query.name : data[id].name,
             "password" : req.query.password ? req.query.password : data[id].password,
             "email" : req.query.email ? req.query.email : data[id].email
-        } 
+        };
         data[id] = json_struct;
         res.set({
             "content-type" : "application/json"
-        })
+        });
         res.end(JSON.stringify(data[id]));
         fs.writeFile("users.json", JSON.stringify(data), "utf8", (err) => console.log(err));
     })
 })
 
-const server = app.listen(3000, () => {
-    console.log("Server has been started.");
+const generate_user_data = (path) => {
+    console.log("\x1b[34m%s\x1b[0m","[GENERATOR] Generating random user data...");
+    const data = fs.readFileSync(path, "utf8");
+    const array = data.split("\n");
+    const length = array.length;
+    const users = {}
+    for (let i = 0; i < length; i++){
+        users[i] = {
+            name: array[Math.floor(Math.random() * length - 1)],
+            password: array[Math.floor(Math.random() * length - 1)],
+            email: array[Math.floor(Math.random() * length - 1)] + "@" + array[Math.floor(Math.random() * length - 1)] + ".com"
+        }
+    }
+    fs.writeFileSync("users.json", JSON.stringify(users));
+}
 
+const server = app.listen(3000, () => {
+    console.log("\x1b[31m%s\x1b[0m","[SERVER] Server has been started at localhost:3000.");
     if (fs.existsSync("users.json") == false) {
-        console.log("users.json not found.");
-        fs.writeFileSync("users.json", "{}");
-        console.log("users.json has been created.");
+        console.log("\x1b[33m%s\x1b[0m", "[SERVER] users.json not found.");
+        const path = "data.txt";
+        if (fs.existsSync(path) == true){
+            console.log("\x1b[36m%s\x1b[0m", "[SERVER] data.txt found.");
+            generate_user_data(path);
+        }
+        else fs.writeFileSync("users.json", "{}");
+        console.log("\x1b[33m%s\x1b[0m","[SERVER] users.json has been created.");
     }
 })
-
